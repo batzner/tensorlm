b35b76e3c2abc1eeb50463b61bcb0ea602fec231
batzner <github@kilians.net>
2017-08-30 12:44:36 +0200

    Changed git tag name from 0.1 to v0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Kilian Batzner",
     author_email="info@kilians.net",
     url="https://github.com/batzner/tensorlm",
-    download_url="https://github.com/batzner/tensorlm/archive/0.1.tar.gz",
+    download_url="https://github.com/batzner/tensorlm/archive/v0.1.tar.gz",
     keywords=["tensorflow", "text", "generation", "language", "model", "rnn", "lstm", "deep",
               "neural", "char", "word"],
     classifiers=[],
